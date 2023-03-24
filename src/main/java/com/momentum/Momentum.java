d469baa94faa385aa94af33e2b5ed2601e6e7da4
pastimee <narutokk0124@gmail.com>
2023-03-24 04:23:50 -0700

    [FIX] Rebase 03/24

--- src/main/java/com/momentum/Momentum.java
+++ src/main/java/com/momentum/Momentum.java
@@ -1,24 +1,6 @@
 package com.momentum;
 
-<<<<<<< Updated upstream
-import com.momentum.api.event.EventBus;
-import com.momentum.impl.handlers.NcpHandler;
-import com.momentum.impl.managers.ChatManager;
-import com.momentum.impl.handlers.CommandHandler;
-import com.momentum.impl.handlers.ModuleHandler;
-import com.momentum.impl.registers.CommandRegistry;
-import com.momentum.impl.registers.ConfigRegistry;
-import com.momentum.impl.registers.ModuleRegistry;
-import com.momentum.impl.init.Configs;
-import com.momentum.impl.handlers.TickHandler;
-import com.momentum.impl.ui.ClickGuiScreen;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-=======
 import com.momentum.api.event.handler.EventHandler;
->>>>>>> Stashed changes
 
 /**
  * Client main
@@ -31,31 +13,6 @@ public class Momentum {
     // event bus
     public static EventHandler EVENT_HANDLER;
 
-<<<<<<< Updated upstream
-    // registries
-    public static ModuleRegistry MODULE_REGISTRY;
-    public static CommandRegistry COMMAND_REGISTRY;
-    public static ConfigRegistry CONFIG_REGISTRY;
-
-    // handler
-    public static ModuleHandler MODULE_HANDLER;
-    public static CommandHandler COMMAND_HANDLER;
-    public static NcpHandler NCP_HANDLER;
-    public static TickHandler TICK_HANDLER;
-
-    // managers
-    public static ChatManager CHAT_MANAGER;
-
-    // click gui
-    public static ClickGuiScreen CLICK_GUI;
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-        // create event bus
-        EVENT_BUS = new EventBus();
-        // logger = event.getModLog();
-=======
     /**
      * Runs
      * during {@link net.minecraftforge.fml.common.event.FMLPreInitializationEvent}
@@ -64,7 +21,6 @@ public class Momentum {
 
         // init event bus
         EVENT_HANDLER = new EventHandler();
->>>>>>> Stashed changes
     }
 
     /**
@@ -73,37 +29,6 @@ public class Momentum {
      */
     public static void init() {
 
-<<<<<<< Updated upstream
-        // initialize registries
-        MODULE_REGISTRY = new ModuleRegistry();
-        COMMAND_REGISTRY = new CommandRegistry();
-        CONFIG_REGISTRY = new ConfigRegistry();
-
-        // initialize handlers
-        MODULE_HANDLER = new ModuleHandler();
-        COMMAND_HANDLER = new CommandHandler();
-        NCP_HANDLER = new NcpHandler();
-        TICK_HANDLER = new TickHandler();
-
-        // initialize managers
-        CHAT_MANAGER = new ChatManager();
-
-        // initialize click gui
-        CLICK_GUI = new ClickGuiScreen();
-
-        // catches exception
-        try {
-
-            // attempt load
-            Configs.DEFAULT_CONFIG.save();
-            Configs.load();
-        }
-
-        // failed to load
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-=======
     }
 
     /**
@@ -112,6 +37,5 @@ public class Momentum {
      */
     public static void postInit() {
 
->>>>>>> Stashed changes
     }
 }
--- src/main/java/com/momentum/impl/ui/ClickGuiScreen.java
+++ src/main/java/com/momentum/impl/ui/ClickGuiScreen.java
@@ -1,14 +1,6 @@
 package com.momentum.impl.ui;
 
 import net.minecraft.client.gui.GuiScreen;
-<<<<<<< Updated upstream
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-=======
->>>>>>> Stashed changes
 
 /**
  *
@@ -18,104 +10,5 @@ public class ClickGuiScreen extends GuiScreen {
     @Override
     public void initGui() {
 
-<<<<<<< Updated upstream
-        // run events on all category frames
-        for (Frame c : frames) {
-
-            // run event
-            c.draw(mouseX, mouseY);
-        }
-
-        // update mouse state
-        MOUSE_LEFT_CLICK = false;
-        MOUSE_RIGHT_CLICK = false;
-
-        // update mouse position
-        MOUSE_X = mouseX;
-        MOUSE_Y = mouseY;
-    }
-
-    @Override
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-
-        // left click
-        if (mouseButton == 0) {
-
-            // update mouse states
-            MOUSE_LEFT_CLICK = true;
-            MOUSE_LEFT_HOLD = true;
-        }
-
-        // right click
-        else if (mouseButton == 1) {
-
-            // update mouse states
-            MOUSE_RIGHT_CLICK = true;
-            MOUSE_RIGHT_HOLD = true;
-        }
-
-        // run events on all category frames
-        for (Frame c : frames) {
-
-            // run event
-            c.click(mouseX, mouseY, mouseButton);
-        }
-    }
-
-    @Override
-    public void mouseReleased(int mouseX, int mouseY, int state) {
-        super.mouseReleased(mouseX, mouseY, state);
-
-        // no longer holding mouse
-        if (state == 0) {
-
-            // update mouse states
-            MOUSE_LEFT_HOLD = false;
-            MOUSE_RIGHT_HOLD = false;
-
-            // run events on all category frames
-            for (Frame c : frames) {
-
-                // release state
-                c.release();
-            }
-        }
-    }
-
-    @Override
-    protected void keyTyped(char p_keyTyped_1_, int p_keyTyped_2_) throws IOException {
-        super.keyTyped(p_keyTyped_1_, p_keyTyped_2_);
-
-        // run events on all category frames
-        for (Frame c : frames) {
-
-            // release state
-            c.type(p_keyTyped_1_, p_keyTyped_2_);
-        }
-    }
-
-    @Override
-    public void onGuiClosed() {
-        super.onGuiClosed();
-
-        // turn off module
-        Modules.CLICKGUI_MODULE.disable();
-    }
-
-    @Override
-    public boolean doesGuiPauseGame() {
-        return false;
-    }
-
-    /**
-     * Gets the frames
-     *
-     * @return The frames
-     */
-    public Collection<Frame> getFrames() {
-        return frames;
-=======
->>>>>>> Stashed changes
     }
 }
